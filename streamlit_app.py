95d72654bda9113025c7824bea0912a6b9a6dce7
Niraj Adhikary <nirajmind@users.noreply.github.com>
2024-05-13 15:28:58 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-from streamlit_extras.colored_header import colored_header
-from streamlit_extras.add_vertical_space import add_vertical_space
 from hugchat import hugchat
 from hugchat.login import Login
 
