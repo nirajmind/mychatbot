dd8f81308d27fa21e2da3e1df5dc91b57aa23cb0
Niraj Adhikary <nirajmind@users.noreply.github.com>
2024-05-13 15:16:03 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,8 @@ def get_text():
 with input_container:
     user_input = get_text()
 # Log in to huggingface and grant authorization to huggingchat
-email= 
-passwd= 
+email =
+passwd =
 sign = Login(email, passwd)
 cookies = sign.login()
 
